bbaaedc910b4f9acc78cf71d866ee4d7e38bf32b
aapsi <aapsi0001@gmail.com>
2023-04-30 18:08:42 +0530

    inheritance

--- contracts/Inheritence.sol
+++ contracts/Inheritence.sol
@@ -1,36 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// is -> enables inheritance
+// Inheritance - where you inherit properties from parent Smart contract
+// Factories - where you deploy smartcontracts within a contract
+// Interaction - when you wanna talk to other smart contracts inside a smart contract function
+
 contract Ownable {
     address owner;
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Must be owner");
-        _;
+    constructor() {
+        owner = msg.sender;
     }
 
-    constructor() public {
-    owner = msg.sender;
-}
-}
-// is -> enables inheritance
-contract MyContract is Ownable {
-    // Inheritance - where you inherit properties from parent Smart contract
-    // Factories - where you deploy smartcontracts within a contract
-    // Interaction - when you wanna talk to other smart contracts inside a smart contract function
-
     modifier onlyOwner() {
-        require(msg.sender == owner, "Must be owner");
+        require(msg.sender == owner, "must be owner");
         _;
     }
+}
 
-    constructor(string memory _secret) public {
+contract SecretVault {
+    string secret;
+
+    constructor(string memory _secret) {
         secret = _secret;
-        super; // it can call the constructor from the parent 
     }
 
-    function getSecret() public view onlyOwner returns(string memory) {
+    function getSecret() public view returns (string memory) {
         return secret;
     }
+}
 
+contract Secret is Ownable {
+    address secretVault;
+
+    constructor(string memory _secret) {
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault = address(_secretVault);
+        super;
+    }
+
+    function getSecret() public view onlyOwner returns (string memory) {
+        return SecretVault(secretVault).getSecret();
+    }
 }
